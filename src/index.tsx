09532c2ecc398b8e5f614d0b84f2b49ca3b042ce
tj-saw <tj@tjsaw.com>
2022-10-12 22:26:01 +0800

    Typescript first files

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { FC } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import Home from "./components/Home";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Route that provides base app UI */}
+        <Route path="/" element={<App />}>
+          {/* Route that renders home content */}
+          <Route index element={<Home />} />
+          {/* Route that renders new listing form
+          <Route path="listings/new" element={<NewListingForm />} /> */}
+          {/* Route that renders individual listings */}
+          {/* <Route path="listings/:listingId" element={<Listing />} /> */}
+          {/* Route that matches all other paths */}
+          <Route path="*" element={"Nothing here!"} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
