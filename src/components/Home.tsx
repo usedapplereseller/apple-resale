604cc9416e099ae6210df61b45f91b3300dada8f
tj-saw <tj@tjsaw.com>
2022-10-18 22:27:58 +0800

    Auth

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,19 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 
 import ListingPreviewList from "./ListingPreviewList";
 
-const Home: React.FC = () => {
+const Home: FC = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <div>
       <Link to="/listings/new">Sell</Link>
       <br />
       <br />
       <ListingPreviewList />
+      <br />
+      <button onClick={() => loginWithRedirect()}>Logout</button>
     </div>
   );
 };
